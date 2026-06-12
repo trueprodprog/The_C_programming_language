541a914c899961eb2d4e13c70b5cbbff8c8fa8ab
Konstantin <ca@Konstantins-MacBook-Pro-2.local>
2026-06-12 17:39:01 +0300

    additional commit

--- additional_test_file.c
+++ additional_test_file.c
@@ -1,15 +1,10 @@
+//
+// Created by Konstantin on 12.06.26.
+//
+
 #include <stdio.h>
 
 main() {
     printf("hello world\n");
-    printf("hello, ");
-    printf("world");
-    printf("\n");
-
-
-
-
     return 0;
-
-
 }
