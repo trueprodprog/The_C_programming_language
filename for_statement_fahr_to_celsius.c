088dc4b57d52a6015d72505275e492beaab1de84
Konstantin <ca@Konstantins-MacBook-Pro-2.local>
2026-06-14 22:05:13 +0300

    for statement

---
+++ for_statement_fahr_to_celsius.c
@@ -0,0 +1,12 @@
+//
+// Created by Konstantin on 14.06.26.
+//
+
+#include <stdio.h>
+/* print Fahrenheit-Celsius table*/
+main() {
+    int fahr;
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
+
